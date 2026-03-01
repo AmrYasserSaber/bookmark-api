834f7d8eb9b88fb2c5f967b811c75eba73eb2236
AmrYasserSaber <amr.yassr.2@gmail.com>
2026-03-01 16:33:17 +0200

    fixed create

--- src/bookmarks/dto/create-bookmark.dto.ts
+++ src/bookmarks/dto/create-bookmark.dto.ts
@@ -1,7 +1,7 @@
 import { Type, type Static } from '@sinclair/typebox';
 
 export const CreateBookmarkSchema = Type.Object({
-  url: Type.String({ format: 'uri' }),
+  url: Type.String({ format: 'url' }),
   title: Type.String(),
   description: Type.Optional(Type.String()),
   tags: Type.Optional(Type.Array(Type.String())),
